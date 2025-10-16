f3e41959aa59b010909a0cd65d23be0e861c9cb6
Yeyul Choi <yeyulchoi2@gmail.com>
2025-10-15 22:01:20 -0400

    install Material-UI and implement Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Navbar from "./app/layout/Navbar";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'cursive',
+  },
+});
 
 export default function App() {
   return (
-    <div>Yeyul's Tinder Web</div>
-  )
+    <Box>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+      </ThemeProvider>
+    </Box>
+  );
 }
-
